c1b67c2c83f56aa26555f15c8dba4c9a7a33ed65
bharath31 <ln.bharath94@gmail.com>
2019-03-01 19:13:57 +0530

    Testing push notifications

--- BranchMonsterFactory/AppDelegate.m
+++ BranchMonsterFactory/AppDelegate.m
@@ -16,6 +16,12 @@
 #import "SplashViewController.h"
 #import "BranchUniversalObject+MonsterHelpers.h"
 
+
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define SYSTEM_VERSION_LESS_THAN(v) ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+
+
+
 @interface AppDelegate ()
 @property (nonatomic) BOOL justLaunched;
 @end
@@ -46,50 +52,62 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
      * the user had just clicked a deep link
      */
 
-    [branch initSessionWithLaunchOptions:launchOptions
-        andRegisterDeepLinkHandlerUsingBranchUniversalObject:
-            ^ (BranchUniversalObject *BUO, BranchLinkProperties *linkProperties, NSError *error) {
+    [[Branch getInstance] initSessionWithLaunchOptions:launchOptions andRegisterDeepLinkHandler:^(NSDictionary * _Nonnull params, NSError * _Nullable error) {
+                
+                
+                UIAlertView *testAlert = [[UIAlertView alloc] initWithTitle:@"Branch initialized. Push notification received."
+                                                                    message:@""
+                                                                   delegate:self
+                                                          cancelButtonTitle:@"Cancel"
+                                                          otherButtonTitles:@"Done", nil];
+                UITextView *textView = [UITextView new];
+                textView.text = [NSString stringWithFormat:@"%@", params];
+                textView.editable = NO;
+                textView.userInteractionEnabled = YES;
 
-                if (linkProperties.controlParams[@"$3p"] &&
-                    linkProperties.controlParams[@"$web_only"]) {
-                    NSURL *url = [NSURL URLWithString:linkProperties.controlParams[@"$original_url"]];
-                    if (url) {
-                        [[NSNotificationCenter defaultCenter]
-                           postNotificationName:@"pushWebView"
-                           object:self
-                           userInfo:@{@"URL": url}];
-                   }
-                } else
-                if (BUO && BUO.contentMetadata.customMetadata[@"monster"]) {
-                    self.initialMonster = BUO;
-                    [[NSNotificationCenter defaultCenter]
-                        postNotificationName:@"pushEditAndViewerViews"
-                        object:nil];
-                } else
-                if (self.justLaunched) {
-                    self.initialMonster = [self emptyMonster];
-                    [[NSNotificationCenter defaultCenter]
-                        postNotificationName:@"pushEditView"
-                        object:nil];
-                    self.justLaunched = NO;
-                }
+                [testAlert setValue: textView forKey:@"accessoryView"];
+                [testAlert show];
 
-                NSDictionary *appleSearchAd = [BNCPreferenceHelper preferenceHelper].appleSearchAdDetails;
-                NSString *campaign = appleSearchAd[@"Version3.1"][@"iad-campaign-name"];
-                if (campaign.length) {
-                    NSLog(@"Got an Apple Search Ad Result :\n%@", appleSearchAd);
-                    /*
-                    NSString *message = [NSString stringWithFormat:@"Campaign: %@", campaign];
-                    UIAlertView *alertView =
-                        [[UIAlertView alloc]
-                            initWithTitle:@"Apple Search Ad Result!"
-                            message:message
-                            delegate:nil
-                            cancelButtonTitle:@"OK"
-                            otherButtonTitles:nil];
-                    [alertView show];
-                    */
-                }
+//                if (linkProperties.controlParams[@"$3p"] &&
+//                    linkProperties.controlParams[@"$web_only"]) {
+//                    NSURL *url = [NSURL URLWithString:linkProperties.controlParams[@"$original_url"]];
+//                    if (url) {
+//                        [[NSNotificationCenter defaultCenter]
+//                           postNotificationName:@"pushWebView"
+//                           object:self
+//                           userInfo:@{@"URL": url}];
+//                   }
+//                } else
+//                if (BUO && BUO.contentMetadata.customMetadata[@"monster"]) {
+//                    self.initialMonster = BUO;
+//                    [[NSNotificationCenter defaultCenter]
+//                        postNotificationName:@"pushEditAndViewerViews"
+//                        object:nil];
+//                } else
+//                if (self.justLaunched) {
+//                    self.initialMonster = [self emptyMonster];
+//                    [[NSNotificationCenter defaultCenter]
+//                        postNotificationName:@"pushEditView"
+//                        object:nil];
+//                    self.justLaunched = NO;
+//                }
+//
+//                NSDictionary *appleSearchAd = [BNCPreferenceHelper preferenceHelper].appleSearchAdDetails;
+//                NSString *campaign = appleSearchAd[@"Version3.1"][@"iad-campaign-name"];
+//                if (campaign.length) {
+//                    NSLog(@"Got an Apple Search Ad Result :\n%@", appleSearchAd);
+//                    /*
+//                    NSString *message = [NSString stringWithFormat:@"Campaign: %@", campaign];
+//                    UIAlertView *alertView =
+//                        [[UIAlertView alloc]
+//                            initWithTitle:@"Apple Search Ad Result!"
+//                            message:message
+//                            delegate:nil
+//                            cancelButtonTitle:@"OK"
+//                            otherButtonTitles:nil];
+//                    [alertView show];
+//                    */
+//                }
             }];
 
     // Optional: Set our own identitier for this user at Branch.
@@ -107,6 +125,33 @@ didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [Localytics autoIntegrate:@"0d738869f6b0f04eb1341f5-fbdada7a-f4ff-11e4-3279-00f82776ce8b"
         launchOptions:launchOptions];
 
+    if( SYSTEM_VERSION_LESS_THAN( @"10.0" ) ) {
+        if (@available(iOS 12.0, *)) {
+            [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound |    UIUserNotificationTypeAlert | UIUserNotificationTypeBadge |  UNAuthorizationOptionProvidesAppNotificationSettings) categories:nil]];
+        } else {
+            // Fallback on earlier versions
+        }
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+        
+        //if( option != nil )
+        //{
+        //    NSLog( @"registerForPushWithOptions:" );
+        //}
+    } else {
+        UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+        center.delegate = self;
+        [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+            if( !error ) {
+                // required to get the app to do anything at all about push notifications
+                [[UIApplication sharedApplication] registerForRemoteNotifications];
+                NSLog( @"Push registration success." );
+            } else {
+                NSLog( @"Push registration FAILED" );
+                NSLog( @"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
+                NSLog( @"SUGGESTIONS: %@ - %@", error.localizedRecoveryOptions, error.localizedRecoverySuggestion );
+            }
+        }];
+    }
     return YES;
 }
 
@@ -139,6 +184,21 @@ continueUserActivity:(NSUserActivity *)userActivity
   restorationHandler:(void (^)(NSArray *))restorationHandler {
     [[Branch getInstance] continueUserActivity:userActivity];
     return YES;
+}
+
+
+- (void)application:(UIApplication*)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    // custom stuff we do to register the device with our AWS middleman
+    NSLog(@"BNC PUSH TOKEN");
+    NSString *token = [[deviceToken description] stringByTrimmingCharactersInSet: [NSCharacterSet characterSetWithCharactersInString:@"<>"]];
+    token = [token stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"content---%@", token);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    // handler for Push Notifications
+    [[Branch getInstance] handlePushNotification:userInfo];
 }
 
 @end
